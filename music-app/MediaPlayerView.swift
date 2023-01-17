80db943d2cd4cd0a6e46f1ccb8b06bdf0ce0e6a1
Cory Ginsberg <cory.ginsberg1@gmail.com>
2023-01-17 16:56:18 -0600

    Added primary and secondary media player icon support with toggle states

--- music-app/MediaPlayerView.swift
+++ music-app/MediaPlayerView.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 
+enum RepeatState {
+  case noRepeat
+  case repeatPlaylist
+  case repeatSong
+}
+
 struct MediaPlayerView: View {
   public var color: Color
   
   @State private var isPlaying = false
-  
   @State private var isPlayingIcon = "play.fill"
+  @State private var repeatIcon = ("repeat", Color(.gray), RepeatState.noRepeat)
+  @State private var isShuffle: Bool = false
   
   var body: some View {
     VStack {
@@ -21,33 +28,12 @@ struct MediaPlayerView: View {
         .frame(height: 500.0)
         .foregroundColor(color)
       HStack {
-        Button(action: prevSong) {
-          Label("Previous", systemImage: "backward.fill")
-            .labelStyle(.iconOnly)
-            .accessibilityAddTraits([.isButton])
-            .accessibilityLabel("Play")
-        }
-          .foregroundColor(color)
-          .padding(.horizontal)
-        Button(action: togglePlay) {
-          Label("Play", systemImage: isPlayingIcon)
-            .labelStyle(.iconOnly)
-            .accessibilityAddTraits([.isButton, .startsMediaSession])
-            .accessibilityLabel("Play")
-        }
-          .foregroundColor(color)
-          .padding(.horizontal)
-        Button(action: nextSong) {
-          Label("", systemImage: "forward.fill")
-            .labelStyle(.iconOnly)
-            .accessibilityAddTraits([.isButton])
-            .accessibilityLabel("Play")
-        }
-          .foregroundColor(color)
-          .padding(.horizontal)
+        MediaSecondaryButton(systemImage: "shuffle", color: isShuffle ? color : Color(.gray), action: self.shuffleSong)
+        MediaPrimaryButton(systemImage: "backward.fill", color: color, action: self.prevSong)
+        MediaPrimaryButton(systemImage: isPlayingIcon, color: color, action: self.togglePlay)
+        MediaPrimaryButton(systemImage: "forward.fill", color: color, action: self.nextSong)
+        MediaSecondaryButton(systemImage: repeatIcon.0, color: repeatIcon.1, action: self.loopSong)
       }
-        .scaledToFit()
-        .scaleEffect(2.0)
         .frame(height: 100.0, alignment: .bottom)
     }
    
@@ -68,6 +54,53 @@ struct MediaPlayerView: View {
   
   func nextSong() -> Void {
     return
+  }
+  
+  func shuffleSong() -> Void {
+    isShuffle.toggle()
+    return
+  }
+  
+  func loopSong() -> Void {
+    switch repeatIcon.2 {
+    case .noRepeat:
+      self.repeatIcon = ("repeat", color, RepeatState.repeatPlaylist)
+    case .repeatPlaylist:
+      self.repeatIcon = ("repeat.1", color, RepeatState.repeatSong)
+    case .repeatSong:
+      self.repeatIcon = ("repeat", Color(.gray), RepeatState.noRepeat)
+    }
+    return
+  }
+}
+
+struct MediaPrimaryButton: View {
+  var systemImage = ""
+  var color: Color
+  var action: () -> Void
+  
+  var body: some View {
+    Button(action: self.action) {
+      Image(systemName: systemImage).font(.largeTitle)
+    }
+    .buttonStyle(.plain)
+    .foregroundColor(color)
+    .frame(maxWidth: 70.0, maxHeight: 40.0)
+  }
+}
+
+struct MediaSecondaryButton: View {
+  var systemImage = ""
+  var color: Color
+  var disabledColor: Color = Color(.gray)
+  var action: () -> Void
+
+  var body: some View {
+    Button(action: self.action) {
+      Image(systemName: systemImage).font(.title3)
+    }
+    .foregroundColor(color)
+    .frame(maxWidth: 50.0)
   }
 }
 
